f7c2c9f7247fa4780b241053cb64d051ebb7495c
nathan <nathankfoss@gmail.com>
2020-03-06 00:53:43 -0600

    Auto stash before merge of "master" and "origin/master"

--- src/app/Header/header.component.ts
+++ src/app/Header/header.component.ts
@@ -5,6 +5,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
 
   // This decorator allows to listen to this event from the parent component.
   // Check out the app component HTML at featureSelected="onNavigate()"

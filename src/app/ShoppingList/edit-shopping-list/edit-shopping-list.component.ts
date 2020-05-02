9ffc923b9660aa45c9a7c4418106c88f84a5ffca
nathan <nathankfoss@gmail.com>
2020-05-02 01:34:28 -0500

    Add form and validation for edit shopping list

--- src/app/ShoppingList/edit-shopping-list/edit-shopping-list.component.ts
+++ src/app/ShoppingList/edit-shopping-list/edit-shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-shopping-list',
@@ -13,28 +14,34 @@ import { ShoppingListService } from '../shopping-list.service';
 
 export class EditShoppingListComponent implements OnInit {
 
+  @ViewChild('ingredientForm', {static:true}) ingredientForm: NgForm;
 
-  @ViewChild('nameInput', {static:false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static:false}) amountInputRef: ElementRef;
-
-  // ==============================================================
-  
   constructor(private shoppinglistService: ShoppingListService) { }
+
+  
 
   ngOnInit() { }
 
   // ==============================================================
   
   onAddIngredient() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, "null", ingAmount);
+    console.log(this.ingredientForm)
+
+    const ingName = this.ingredientForm.value.itemName;
+    const ingAmount = this.ingredientForm.value.amount;
+    const ingUnit = this.ingredientForm.value.unit;
+    const newIngredient = new Ingredient(ingName, ingUnit, ingAmount);
     this.shoppinglistService.addIngredient(newIngredient)
 
+    this.ingredientForm.reset();
+
+    // ~~OLD~~
     // Now reset the fields. (I know I'm not supposed to access the elements directly like this)
-    this.nameInputRef.nativeElement.value = "";
-    this.amountInputRef.nativeElement.value = "";
+    // this.nameInputRef.nativeElement.value = "";
+    // this.amountInputRef.nativeElement.value = "";
   }
+
+
 
   
 

e365018a21acdde982d5560babf7484b8edbd3e6
nathan <nathankfoss@gmail.com>
2020-05-15 16:33:58 -0500

    Fix bug with resolver overwriting recipes

--- src/app/RecipeBook/recipes-resolver.service.ts
+++ src/app/RecipeBook/recipes-resolver.service.ts
@@ -4,13 +4,22 @@ import { Recipe } from './recipe.model';
 import { RecipeService } from './recipes.service';
 
 // The resolver runs before a route is loaded, to make sure that certain data the route depends on, is available.
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]>{
 
-    constructor(private recipeService: RecipeService) {}
+    constructor(private recipeService: RecipeService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.recipeService.fetchRecipes();
+        // We should only use the resolver if there are actually no recipes in the recipe service.
+        // We get the recipes and then check the length. If it is greater than 0, then we simply return the recipes.
+        // Else, we use the resolver.
+        // This is done so the resolver doesn't overwrite any edits we make/submit.
+        const recipes = this.recipeService.getRecipes()
+        if (recipes.length < 1) {
+            return this.recipeService.fetchRecipes();
+        } else {
+            return recipes
+        }
         // The resolver will subscribe for you. No need to do it explciitally.
     }
 }

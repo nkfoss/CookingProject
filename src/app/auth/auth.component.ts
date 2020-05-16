1f8b2211a631889c6fe5ed159b81ce7a69600fb0
nathan <nathankfoss@gmail.com>
2020-05-16 15:08:22 -0500

    Add button to switch login modes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
     templateUrl: './auth.component.html'
 })
 export class AuthComponent{
+    isLoginMode = true;
 
+    onSwitchMode() { this.isLoginMode = !this.isLoginMode}
 }

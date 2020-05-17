9407aa0c657d67a49a840dfafdaa6dbc20e299be
nathan <nathankfoss@gmail.com>
2020-05-16 21:04:36 -0500

    Combine error handling for login/signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 
 
@@ -22,23 +23,31 @@ export class AuthComponent{
         console.log(this.isLoginMode)
     }
 
+
     onSubmit(form: NgForm) {
 
+        this.error = null
         if (!form.valid) { return;}
+
+        let authObs: Observable<AuthResponseData>;
 
         this.isLoading = true;
         if (this.isLoginMode) {
-            //...
+            authObs = this.authService.signIn(form.value.email, form.value.password)
         } else {
-            this.authService.signUp(form.value.email, form.value.password).subscribe( resData => {
-                this.isLoading = false;
-                console.log(resData)
-            }, errorMessage => {
-                this.isLoading = false;
-                this.error = errorMessage;
-                console.log(errorMessage)
-            })
+            authObs =this.authService.signUp(form.value.email, form.value.password)
         }
+        authObs.subscribe( resData => {
+            this.isLoading = false;
+            console.log(resData)
+        }, errorMessage => {
+            this.isLoading = false;
+            this.error = errorMessage;
+            console.log(errorMessage)
+            
+        })
+
         form.reset(); // Always reset the form.
     }
+    
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,54 +1,78 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import credentials from '../../../credentials/credentials.json';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { EmailValidator } from '@angular/forms';
 
-interface AuthResponseData {   // We define the interface here, since we only need it here. Also, interface's are good practice.
-    // This is the response payload's structure.
-    kind: string,
+export interface AuthResponseData {   // We define the interface here, since we only need it here. Also, interface's are good practice.
+    // This is the response payload's structure. There are optional fields, because the exact structure depends on what request is being responded to.
+    // https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+    kind?: string,
     idToken: string,
     email: string,
     refreshToken: string,
     expiresin: string,
-    localid: string
+    localid: string,
+    registered?: boolean
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
 
-constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-signUp(email: string, password: string) {
+    signUp(email: string, password: string) {
 
-    const API_KEY = credentials.firebaseAPI;
-    console.log(API_KEY);
-    return this.http.post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-        // The API key can be retrieved from your Firebase project settings.
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-            // This is the structure of the request payload
-        }
-    ).pipe(catchError(errorRes => {
+        const API_KEY = credentials.firebaseAPI;
+        console.log(API_KEY);
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+            // The API key can be retrieved from your Firebase project settings.
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+                // This is the structure of the request payload
+            }
+        ).pipe(catchError(this.handleErrorResponse))
+
+    }
+
+    signIn(email: string, password: string) {
+        const API_KEY = credentials.firebaseAPI;
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + API_KEY,
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleErrorResponse))
+    }
+
+    private handleErrorResponse(errorRes: HttpErrorResponse) {
         let errorMessage = "An unknown error occurred"
-        if (!errorRes.error || !errorRes.error.error) {
+            console.log(errorRes)
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage)
+            }
+            switch (errorRes.error.error.message) {
+                case 'INVALID_PASSWORD':
+                    errorMessage = "Email or password is incorrect";
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = "Email or password is incorrect";
+                    break;
+                case 'EMAIL_EXISTS':
+                    errorMessage = "This email is already in-use";
+                    break;
+                case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                    errorMessage = "Too many failed attempts. Please try again later."
+                    break;
+            }
             return throwError(errorMessage)
-        }
-        switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-                errorMessage = "This email already exists"
-        }
-        return throwError(errorMessage)
-    }))
-    
-}
-
-signIn() {
-
-}
+    }
 
 }
